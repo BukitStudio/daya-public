9f303064621f48686d46d94b3c8750ca88d3882f
BukitStudio <contact@thebukitstudio.com>
2025-07-22 08:59:56 +0800

    pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+import { imageService } from "@unpic/astro/service";
+
+
+export default defineConfig({
+  site: 'https://soberstudio.dev',
+  output: 'static',
+   devToolbar: {
+    enabled: false,
+  },
+  image: {
+    service: imageService(
+      {  placeholder: "blurhash",
+      // This is the default
+      layout: "constrained",}
+    ),
+  },
+
+})
