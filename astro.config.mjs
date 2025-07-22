0c743a89ddcb764b0f0ba5ff8fe001b41efcf83a
BukitStudio <contact@thebukitstudio.com>
2025-07-23 00:47:12 +0800

    day2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from "astro/config";
 
-import { imageService } from "@unpic/astro/service";
-
 
 export default defineConfig({
   site: 'https://soberstudio.dev',
@@ -9,12 +7,6 @@ export default defineConfig({
    devToolbar: {
     enabled: false,
   },
-  image: {
-    service: imageService(
-      {  placeholder: "blurhash",
-      // This is the default
-      layout: "constrained",}
-    ),
-  },
+
 
 })
